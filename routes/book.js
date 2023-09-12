ee7ce351d47342f0aa0fec5cd7bf6c4c6e78fc7e
Chloé <125600895+Ch1oeee@users.noreply.github.com>
2023-09-12 14:00:51 +0200

    Modification et suppression des livres

--- routes/book.js
+++ routes/book.js
@@ -10,7 +10,7 @@ const bookCtrl = require('../controllers/book');
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', auth, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
